314796ce27a106f1e52076bfc141b08a0814cc0c
John Z <john.zissimou@and.digital>
2024-10-20 21:58:32 +0100

    - Created a Service, Interface and Controller for Spaces. - Created a Response Helper to deal with the API responses. - Added a create space endpoint. - Removed minimal API from Program.cs and added necessary code for the HTTP requests. - Added URLS of port 80 in Dockerfiles. - Changed connection string name in docker compose so dev and preprod. - Matched the local docker compose to match the dev and preprod. - Added a .env.local.

---
+++ Controllers/SpaceController.cs
@@ -0,0 +1,25 @@
+using EasyContinuity_API.Helpers;
+using EasyContinuity_API.Interfaces;
+using EasyContinuity_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyContinuity_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpaceController : ControllerBase
+    {
+        private readonly ISpaceService _spaceService;
+
+        public SpaceController(ISpaceService spaceService)
+        {
+            _spaceService = spaceService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Space>> Create(Space space)
+        {
+            return ResponseHelper.HandleErrorAndReturn(await _spaceService.CreateSpace(space));
+        }
+    }
+}
