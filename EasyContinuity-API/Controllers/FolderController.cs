ad6c3071c61557e7b3c6f17c3e52b862288eca36
John Z <john.zissimou@and.digital>
2024-10-25 12:56:02 +0100

    Created Folder controller and services and added the Create endpoint.

---
+++ EasyContinuity-API/Controllers/FolderController.cs
@@ -0,0 +1,25 @@
+using EasyContinuity_API.Helpers;
+using EasyContinuity_API.Interfaces;
+using EasyContinuity_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyContinuity_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FolderController : ControllerBase
+    {
+        private readonly IFolderService _folderService;
+
+        public FolderController(IFolderService folderService)
+        {
+            _folderService = folderService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Folder>> Create(Folder folder)
+        {
+            return ResponseHelper.HandleErrorAndReturn(await _folderService.CreateFolder(folder));
+        }
+    }
+}
