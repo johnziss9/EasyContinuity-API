a7fd02f2e53bd56b13b07374de3e15f76bec2653
John Z <john.zissimou@and.digital>
2024-10-28 21:51:10 +0000

    Added endpoint for updating snapshots. Added condition for updated deletedBy property on folder and space update endpoint.

--- EasyContinuity-API/Services/SnapshotService.cs
+++ EasyContinuity-API/Services/SnapshotService.cs
@@ -56,49 +56,133 @@ namespace EasyContinuity_API.Services
             return Response<Snapshot>.Success(snapshot);
         }
 
-        // public async Task<Response<Space>> UpdateSpace(int id, Space updatedSpace)
-        // {
-        //     var space = await _ecDbContext.Spaces.Where(s => s.Id == id).FirstOrDefaultAsync();
+        public async Task<Response<Snapshot>> UpdateSnapshot(int id, Snapshot updatedSnapshot)
+        {
+            var snapshot = await _ecDbContext.Snapshots.Where(s => s.Id == id).FirstOrDefaultAsync();
 
-        //     if (space == null)
-        //     {
-        //         return Response<Space>.Fail(404, "Space Not Found");
-        //     }
+            if (snapshot == null)
+            {
+                return Response<Snapshot>.Fail(404, "Snapshot Not Found");
+            }
 
-        //     if (updatedSpace.Name != null && updatedSpace.Name != space.Name)
-        //     {
-        //         space.Name = updatedSpace.Name;
-        //     }
+            if (updatedSnapshot.Name != null && updatedSnapshot.Name != snapshot.Name)
+            {
+                snapshot.Name = updatedSnapshot.Name;
+            }
 
-        //     if (updatedSpace.Description != null && updatedSpace.Description != space.Description)
-        //     {
-        //         space.Description = updatedSpace.Description;
-        //     }
+            if (updatedSnapshot.FolderId != null && updatedSnapshot.FolderId != snapshot.FolderId)
+            {
+                snapshot.FolderId = updatedSnapshot.FolderId;
+            }
 
-        //     if (updatedSpace.IsDeleted != space.IsDeleted)
-        //     {
-        //         space.IsDeleted = updatedSpace.IsDeleted;
-        //     }
+            if (updatedSnapshot.IsDeleted != snapshot.IsDeleted)
+            {
+                snapshot.IsDeleted = updatedSnapshot.IsDeleted;
+            }
 
-        //     if (updatedSpace.LastUpdatedBy != null && updatedSpace.LastUpdatedBy != space.LastUpdatedBy)
-        //     {
-        //         space.LastUpdatedBy = updatedSpace.LastUpdatedBy;
-        //     }
+            if (updatedSnapshot.LastUpdatedBy != null && updatedSnapshot.LastUpdatedBy != snapshot.LastUpdatedBy)
+            {
+                snapshot.LastUpdatedBy = updatedSnapshot.LastUpdatedBy;
+            }
 
-        //     if (updatedSpace.LastUpdatedOn != null && updatedSpace.LastUpdatedOn != space.LastUpdatedOn)
-        //     {
-        //         space.LastUpdatedOn = updatedSpace.LastUpdatedOn;
-        //     }
+            if (updatedSnapshot.LastUpdatedOn != null && updatedSnapshot.LastUpdatedOn != snapshot.LastUpdatedOn)
+            {
+                snapshot.LastUpdatedOn = updatedSnapshot.LastUpdatedOn;
+            }
 
-        //     if (updatedSpace.DeletedOn != null && updatedSpace.DeletedOn != space.DeletedOn)
-        //     {
-        //         space.DeletedOn = updatedSpace.DeletedOn;
-        //     }
+            if (updatedSnapshot.DeletedOn != null && updatedSnapshot.DeletedOn != snapshot.DeletedOn)
+            {
+                snapshot.DeletedOn = updatedSnapshot.DeletedOn;
+            }
 
-        //     await _ecDbContext.SaveChangesAsync();
+            if (updatedSnapshot.DeletedBy != null && updatedSnapshot.DeletedBy != snapshot.DeletedBy)
+            {
+                snapshot.DeletedBy = updatedSnapshot.DeletedBy;
+            }
 
-        //     return Response<Space>.Success(space);
-        // }
+            if (updatedSnapshot.Episode != null && updatedSnapshot.Episode != snapshot.Episode)
+            {
+                snapshot.Episode = updatedSnapshot.Episode;
+            }
 
+            if (updatedSnapshot.Scene != null && updatedSnapshot.Scene != snapshot.Scene)
+            {
+                snapshot.Scene = updatedSnapshot.Scene;
+            }
+
+            if (updatedSnapshot.StoryDay != null && updatedSnapshot.StoryDay != snapshot.StoryDay)
+            {
+                snapshot.StoryDay = updatedSnapshot.StoryDay;
+            }
+
+            if (updatedSnapshot.Character != null && updatedSnapshot.Character != snapshot.Character)
+            {
+                snapshot.Character = updatedSnapshot.Character;
+            }
+
+            if (updatedSnapshot.Notes != null && updatedSnapshot.Notes != snapshot.Notes)
+            {
+                snapshot.Notes = updatedSnapshot.Notes;
+            }
+
+            if (updatedSnapshot.Skin != null && updatedSnapshot.Skin != snapshot.Skin)
+            {
+                snapshot.Skin = updatedSnapshot.Skin;
+            }
+
+            if (updatedSnapshot.Brows != null && updatedSnapshot.Brows != snapshot.Brows)
+            {
+                snapshot.Brows = updatedSnapshot.Brows;
+            }
+
+            if (updatedSnapshot.Eyes != null && updatedSnapshot.Eyes != snapshot.Eyes)
+            {
+                snapshot.Eyes = updatedSnapshot.Eyes;
+            }
+
+            if (updatedSnapshot.Lips != null && updatedSnapshot.Lips != snapshot.Lips)
+            {
+                snapshot.Lips = updatedSnapshot.Lips;
+            }
+
+            if (updatedSnapshot.Effects != null && updatedSnapshot.Effects != snapshot.Effects)
+            {
+                snapshot.Effects = updatedSnapshot.Effects;
+            }
+
+            if (updatedSnapshot.MakeupNotes != null && updatedSnapshot.MakeupNotes != snapshot.MakeupNotes)
+            {
+                snapshot.MakeupNotes = updatedSnapshot.MakeupNotes;
+            }
+
+            if (updatedSnapshot.Prep != null && updatedSnapshot.Prep != snapshot.Prep)
+            {
+                snapshot.Prep = updatedSnapshot.Prep;
+            }
+
+            if (updatedSnapshot.Method != null && updatedSnapshot.Method != snapshot.Method)
+            {
+                snapshot.Method = updatedSnapshot.Method;
+            }
+
+            if (updatedSnapshot.StylingTools != null && updatedSnapshot.StylingTools != snapshot.StylingTools)
+            {
+                snapshot.StylingTools = updatedSnapshot.StylingTools;
+            }
+
+            if (updatedSnapshot.Products != null && updatedSnapshot.Products != snapshot.Products)
+            {
+                snapshot.Products = updatedSnapshot.Products;
+            }
+
+            if (updatedSnapshot.HairNotes != null && updatedSnapshot.HairNotes != snapshot.HairNotes)
+            {
+                snapshot.HairNotes = updatedSnapshot.HairNotes;
+            }
+
+            await _ecDbContext.SaveChangesAsync();
+
+            return Response<Snapshot>.Success(snapshot);
+        }
     }
 }
